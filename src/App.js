c5508181b03ee45e5661717448c977c23c378da7
Abhishek Jha <imajabhishekjha333@gmail.com>
2023-07-14 01:37:07 +0530

    building current weather component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import Search from './components/search/search';
+import CurrentWeather from './components/search/current-weather/current-weather';
+
 
 function App() {
   const handleOnSearchChange= (searchData) => {
@@ -8,6 +10,7 @@ function App() {
   return (
     <div className="container">
       <Search onSearchChange={handleOnSearchChange} />
+      <CurrentWeather/>
     </div>
   );
 }
