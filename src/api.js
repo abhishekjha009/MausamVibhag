b01d703d486486322de92437964037dd90ee0e72
Abhishek Jha <imajabhishekjha333@gmail.com>
2023-07-18 14:21:09 +0530

    fetching and mapping data from weather api and building weather forecast component

--- src/api.js
+++ src/api.js
@@ -1,9 +1,11 @@
 export const geoApiOptions = {
-	method: 'GET',
+	method: "GET",
 	headers: {
-		'X-RapidAPI-Key': 'ab77d28afdmsh65f49aa995428ffp16f371jsnaa282f2779a9',
-		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
-	}
-};
-export const GEO_API_URL= "https://wft-geo-db.p.rapidapi.com/v1/geo";
-
+	  "X-RapidAPI-Key": "ab77d28afdmsh65f49aa995428ffp16f371jsnaa282f2779a9",// enter your rapid api key here
+	  "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+  };
+  export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+  
+  export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+  export const WEATHER_API_KEY = "373b8d85e1df76bb6cc8620057308dc9"; // enter your key from openweather API
--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,41 +1,42 @@
+import React, { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import { useState } from "react";
-import {GEO_API_URL, geoApiOptions} from '../../api';
-const Search = ({onSearchChange}) => {
+import { geoApiOptions, GEO_API_URL } from "../../api";
 
-    const [search, setSearch] = useState(null);
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
 
-    const handleOnChange= (searchData) => {
-        setSearch(searchData);
-        onSearchChange(searchData);
-    }
-
-    const loadOptions= (inputValue) => {
-        return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoApiOptions)
-        .then((response)=> response.json())
-        .then((response) => {
-            console.log(response)
-            return{
-                options: response.data.map((city)=>{
-                    return{
-                        value:`${city.latitude}, ${city.longitude}` ,
-                        label: `${city.name}, ${city.countryCode}`,
-                    };
-                }),
+  const loadOptions = (inputValue) => {
+    return fetch(
+      `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`,
+      geoApiOptions
+    )
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitude} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
             };
-        })
-        .catch((err) =>  console.error(err));
-    };
+          }),
+        };
+      });
+  };
 
-    return(
-        <AsyncPaginate
-           placeholder="Search for city"
-           debounceTimeout={600}
-           value={search}
-           onChange={handleOnChange}
-           loadOptions={loadOptions}
-        />
-        )
-}
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  };
+
+  return (
+    <AsyncPaginate
+      placeholder="Search for city"
+      debounceTimeout={600}
+      value={search}
+      onChange={handleOnChange}
+      loadOptions={loadOptions}
+    />
+  );
+};
 
 export default Search;
